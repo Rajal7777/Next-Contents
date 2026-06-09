3f454700e3576f8c7868a032f8529160b2df24d6
Rajal7777 <suwalrajal57@gmail.com>
2026-06-09 21:44:19 +0900

    POST req

--- next-curd-mongodb/app/api/users/route.js
+++ next-curd-mongodb/app/api/users/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { connectDB } from "@/lib/mongodb";
 
+//get method
 export async function GET() {
   const db = await connectDB();
 
@@ -8,5 +9,20 @@ export async function GET() {
 
   return NextResponse.json({
     users,
+  });
+}
+
+//post method
+export async function POST(req) {
+  const db = await connectDB();
+
+  //get data from users
+  const body = await req.json();
+  //insert data in DB
+  const userData = await db.collection("users").insertOne(body);
+
+  return NextResponse.json({
+    message: "Successful",
+    data: userData,
   });
 }

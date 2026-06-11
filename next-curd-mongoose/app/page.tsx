4d6961365241fb2c32a958ba0eeb4a05a0c00801
Rajal7777 <suwalrajal57@gmail.com>
2026-06-11 19:52:56 +0900

    edit function added

--- next-curd-mongoose/app/page.tsx
+++ next-curd-mongoose/app/page.tsx
@@ -5,66 +5,124 @@ export default function Home() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [users, setUsers] = useState([]);
+  const [editId, setEditId] = useState(null);
 
-  //Get users
+  console.log("data", users);
+  console.log("id", editId);
+
+ //Get users
   const fetchUser = async () => {
-    const res = await fetch('/api/users');
+    const res = await fetch("/api/users");
     const data = await res.json();
     setUsers(data.data);
- };
+  };
 
+  //Load data
   useEffect(() => {
     fetchUser();
   }, []);
 
+  //Create or update
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const res = await fetch("api/users", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ name, email }),
-    });
+    //update
+    if (editId) {
+      await fetch(`/api/users/${editId}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, email }),
+      });
 
-    const data = await res.json();
+      setEditId(null);
+      alert("edited successfully");
 
-    alert("User created successfully");
-    setName("");
-    setEmail("");
+      setName("");
+      setEmail("");
+
+      fetchUser();
+    } else {
+      const res = await fetch("/api/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, email }),
+      });
+      const data = await res.json();
+
+      alert("User created successfully");
+
+      setName("");
+      setEmail("");
+
+      fetchUser();
+    }
   };
 
+  //Edit
+  const handleEdit = (user) => {
+    setName(user.name);
+    setEmail(user.email);
+    setEditId(user._id);
+  };
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+
+  //   const res = await fetch("api/users", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({ name, email }),
+  //   });
+
+  //   const data = await res.json();
+  //   console.log(data);
+  //   alert("User created successfully");
+  //   setName("");
+  //   setEmail("");
+  // };
+
   return (
-    <div className="flex flex-col  justify-center items-center ">
-      <h1>Create user</h1>
-      <form onSubmit={handleSubmit}
-        className="text-center">
+    <div>
+      <h1>{editId ? "Edit User" : "Create user"}</h1>
+      <form onSubmit={handleSubmit} >
         <input
-          className="text-center p-2 border"
-          type="text"
+         type="text"
           placeholder="Enter name"
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
         <br /> <br />
+
         <input
-          className="text-center p-2 border"
           type="text"
           placeholder="Enter email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-        <br /> <br />
-        <button
-          className="border py-2 px-5"
-          type="submit">Submit form</button>
+         <br /> <br />
+
+        <button className="border py-2 px-5" type="submit">
+          {editId ? "Edit User" : "Create user"}
+        </button>
       </form>
-      <hr />
+
       <div>
         <h2>Users Contact</h2>
         {users.map((user) => (
-          <div key={user._id}>{user.name}-{user.email}</div>
+          <div key={user._id}>
+            {user.name}-{user.email}
+            <button 
+            className="ml-6"
+            onClick={() => handleEdit(user)}>
+              edit button
+            </button>
+          </div>
         ))}
       </div>
     </div>
